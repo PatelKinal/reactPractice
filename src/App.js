3bfb21584e6a1fde22f10a181bd34b7e2963d548
PatelKinal <kinalgoti@gmail.com>
2023-06-04 01:20:39 +1000

    Done: use useState hook for increment and decrement value

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import "./App.css";
-import Header from "./components/Header";
-import Movie from "./components/Movie";
+
 import movies from "./movie.json";
+import React, { useState } from "react";
+import Movie from "./components/Movie";
+import Header from "./components/Header";
+import { func } from "prop-types";
 
 function App() {
+  const [element, setElement] = useState(1);
+
+  function inc(n) {
+    setElement(element + n);
+  }
+
+  function dec() {
+    setElement(element - 1);
+  }
+
   return (
     <div className="App">
       <Header />
       <div className="main">
-        {movies.map((element, index) => {
+        {/* {movies.map((element, index) => {
           return (
             <Movie
               key={index}
@@ -17,7 +30,17 @@ function App() {
               img={element.poster}
             />
           );
-        })}
+        })} */}
+
+        <h1 className="heading">{element}</h1>
+        <div className="buttons">
+          <button className="btn" onClick={() => inc(2)}>
+            Increment
+          </button>
+          <button className="btn" onClick={dec}>
+            Decrement
+          </button>
+        </div>
       </div>
     </div>
   );
